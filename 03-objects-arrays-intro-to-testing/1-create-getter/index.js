a33c660034c49c4a223bca49b0c8014ae1a6e0b5
Timur Anosov <timur@capusta.space>
2021-06-23 15:03:44 +0300

    Finished lesson 3 tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,20 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  let names = path.split(".");
+  return function foo(obj) {
+    let innerObj = Object.assign({}, obj);
+    for (name of names) {
+      if (!innerObj.hasOwnProperty(name)) {
+        return undefined;
+      }
+      innerObj = getObjByName(innerObj, name);
 
+      function getObjByName(obj, name) {
+        innerObj = innerObj[name];
+        return innerObj;
+      }
+    }
+    return innerObj;
+  };
 }
