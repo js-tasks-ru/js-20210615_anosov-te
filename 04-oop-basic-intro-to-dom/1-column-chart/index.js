d5091346edd1bb9c864735300b2656f533a922ab
Timur Anosov <timur@capusta.space>
2021-06-29 13:45:17 +0300

    Fixes after review

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,26 +1,17 @@
 export default class ColumnChart {
-  constructor(params) {
-    if (params === undefined) {
-      params = {
-        label: "",
-        link: "",
-        value: 0,
-        data: [],
-        formatHeading: function () {
-          return 0;
-        },
-        chartHeight: 50
-      };
-    }
+  constructor({
+                data = [], label = '', link = '', value = 0, formatHeading = data => data,
+                chartHeight = 50
+              } = {}) {
+    this.label = label;
+    this.value = value;
+    this.link = link;
+    this.data = data;
+    this.chartHeight = chartHeight;
+    this.formatHeading = function (data) {
+      return formatHeading(data);
+    };
 
-    this.label = params.label === undefined ? "" : params.label;
-    this.value = params.value === undefined ? 0 : params.value;
-    this.link = params.link === undefined ? "" : params.link;
-    this.data = params.data === undefined ? [] : params.data;
-    this.chartHeight = params.chartHeight === undefined ? 50 : params.chartHeight;
-    this.formatHeading = params.formatHeading === undefined ? function () {
-      return params.value;
-    } : params.formatHeading;
     this.render();
   }
 
@@ -29,34 +20,7 @@ export default class ColumnChart {
     element.classList.add('column-chart');
     element.setAttribute("style", "--chart-height: " + this.chartHeight);
 
-    if (this.data.length === 0) {
-      element.classList.add('column-chart_loading');
-      element.innerHTML = `
-          <div class="column-chart__title">
-            ${this.label}
-            <a class="column-chart__link" href="${this.link}">View all</a>
-          </div>
-          <div class="column-chart__container">
-            <div data-element="header" class="column-chart__header">
-              ${this.formatHeading(this.value)}
-            </div>
-            <div data-element="body" class="column-chart__chart">
-
-            </div>
-          </div>
-      `;
-    } else {
-      const chartProps = this.getColumnProps(this.data);
-
-      let charts = document.createDocumentFragment();
-      for (let item of chartProps) {
-        let itemDiv = document.createElement('div');
-        itemDiv.setAttribute("style", "--value: " + item.value);
-        itemDiv.setAttribute("data-tooltip", item.percent);
-        charts.appendChild(itemDiv);
-      }
-
-      element.innerHTML = `
+    element.innerHTML = `
         <div class="column-chart__title">
           ${this.label}
           <a href="${this.link}" class="column-chart__link">View all</a>
@@ -68,7 +32,20 @@ export default class ColumnChart {
         </div>
     `;
 
-      element.querySelector('.column-chart__chart').appendChild(charts);
+    const chartProps = this.getColumnProps(this.data);
+
+    let charts = document.createDocumentFragment();
+    for (let item of chartProps) {
+      let itemDiv = document.createElement('div');
+      itemDiv.setAttribute("style", "--value: " + item.value);
+      itemDiv.setAttribute("data-tooltip", item.percent);
+      charts.append(itemDiv);
+    }
+
+    element.querySelector('.column-chart__chart').append(charts);
+
+    if (this.data.length === 0) {
+      element.classList.add('column-chart_loading');
     }
 
     this.element = element;
